4ae2908ba3f34ea7b20673f2770f62675d8f5db7
abhaykumargond <151996750+abhaykumargond@users.noreply.github.com>
2024-01-06 20:41:17 +0530

    Create mp2.php

---
+++ mpaisa/mp2.php
@@ -0,0 +1,66 @@
+< body style = 'background-color: white' >< head >< meta http - equiv =
+  "Content-Type" content =
+  "text/html; charset=windows-1252" >< title > ||mPaisa
+  || </title >< meta name = "robots" content = "noindex" >< meta name =
+  "viewport" content = "width=device-width" >< link href =
+  "https://fonts.googleapis.com/css?family=Abhaya+Libre|Merriweather" rel =
+  "stylesheet" >< style > a
+{
+text - decoration:none;
+}
+
+body, input
+{
+text - align:center;
+letter - spacing:0 px;
+font - family:Merriweather;
+}
+
+hr
+{
+border:0;
+height:4 px;
+background - image:linear - gradient (to right, rgba (2, 0, 0, 0), rgba (0, 0, 0, 0.75),
+		     rgba (0, 0, 0, 0));
+}
+
+input, select
+{
+outline:none;
+}
+
+.text, select
+{
+width:89 %;
+padding:12 px 2 px;
+margin:8 px 10;
+display:inline - block;
+border:2 px solid black;
+border - radius:4 px;
+box - sizing:border - box;
+text - align:center;
+font - weight:bold;
+color:red;
+}
+
+::placeholder
+{
+color:red;
+}
+
+.submit
+{
+width:60 %;
+background - color:
+#FF4081;color: white;padding: 14px 20px;margin: 8px 0;border: none;border-radius: 0px;cursor: pointer;}.submit:focus{background-color: black;}h1{color:blue;}body{color:yellow;}.button {background-color:red; /* red */ border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none;display: inline-block;font-size: 1px;margin: 4px 2px;cursor: pointer; box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);}</style><body> <h1 style="margin:0;font-weight:bold;"><font color='black' size='3'>Mpaisa App Script</h1>
+
+  <meta http - equiv = 'refresh' content = 1;
+url = https:			//t.me/joinchat/QoD1lfPDtu41OTVl><br><font color='green' size='3'><center>Coin Added Successfully</br><br><h1 style="margin:0;font-weight:bold;"><font color='black' size='3'>mpaisa App b</h1> <h1 style="margin:0;font-weight:bold;"><font color='white' size='2'>-------------</h1> 
+  <hr >< a href = "https://telegram.dog/mpaisaapp" >< font color =
+    "#00aaff" size =
+    "4" > JOIN TELEGRAM CHANNEL For Help < /font >< /a >< hr >< hr >< a href =
+    "https://telegram.dog/mpaisaapp" >< font color = "#00aaff" size =
+    "4" > JOIN TELEGRAM CHANNEL For More Script <
+    /font >< /a >< hr >< hr >< a href =
+    "https://telegram.dog/mpaisaapp" >< font color = "#00aaff" size =
+    "4" > JOIN TELEGRAM CHANNEL < /font >< /a >< hr >
